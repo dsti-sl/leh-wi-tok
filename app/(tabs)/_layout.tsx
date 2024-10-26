6f5e0be020535f45855404b725b5d5bed3918f64
ShekuMunu <munusheku@gmail.com>
2024-10-26 16:37:22 +0000

    update nav tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,20 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import TabBarIcon from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
 
-export default function TabLayout() {
-  // TODO: Apply styling to tabs elements
+export default function Layout() {
   return (
     <Tabs
       screenOptions={{
+        tabBarShowLabel: true,
+        tabBarStyle: styles.tabBar,
+        tabBarItemStyle: styles.tabBarItemStyle,
+        tabBarLabelStyle: styles.tabLabel,
+        tabBarActiveTintColor: Colors.primary,
+        tabBarInactiveTintColor: '#9E9E9E',
         headerShown: false,
       }}
     >
@@ -17,8 +24,12 @@ export default function TabLayout() {
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
-              name={focused ? 'home' : 'home-outline'}
+              tabStyle={styles.iconContainer}
+              activeStyle={styles.activeTab}
+              activeIcon={'home' as 'text'}
+              icon={'home-outline' as 'text'}
               color={color}
+              focused={focused}
             />
           ),
         }}
@@ -29,8 +40,12 @@ export default function TabLayout() {
           title: 'Lessons',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
-              name={focused ? 'code-slash' : 'code-slash-outline'}
+              tabStyle={styles.iconContainer}
+              activeStyle={styles.activeTab}
+              activeIcon={'book' as 'text'}
+              icon={'book-outline' as 'text'}
               color={color}
+              focused={focused}
             />
           ),
         }}
@@ -41,8 +56,12 @@ export default function TabLayout() {
           title: 'Dictionary',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
-              name={focused ? 'code-slash' : 'code-slash-outline'}
+              tabStyle={styles.circularIconContainer}
+              activeStyle={styles.activeTab}
+              activeIcon={'book' as 'text'}
+              icon={'book-outline' as 'text'}
               color={color}
+              focused={focused}
             />
           ),
         }}
@@ -53,8 +72,12 @@ export default function TabLayout() {
           title: 'Tok',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
-              name={focused ? 'code-slash' : 'code-slash-outline'}
+              tabStyle={styles.iconContainer}
+              activeStyle={styles.activeTab}
+              activeIcon={'chatbubble' as 'text'}
+              icon={'chatbubble-outline' as 'text'}
               color={color}
+              focused={focused}
             />
           ),
         }}
@@ -65,8 +88,12 @@ export default function TabLayout() {
           title: 'Account',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
-              name={focused ? 'code-slash' : 'code-slash-outline'}
+              tabStyle={styles.iconContainer}
+              activeStyle={styles.activeTab}
+              activeIcon={'person' as 'text'}
+              icon={'person-outline' as 'text'}
               color={color}
+              focused={focused}
             />
           ),
         }}
@@ -74,3 +101,42 @@ export default function TabLayout() {
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBar: {
+    borderRadius: 20,
+    backgroundColor: '#f9f9f9',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0,
+    borderColor: '#f9f9f9',
+    paddingTop: 10,
+  },
+  tabBarItemStyle: { bottom: 5, top: 0 },
+  tabLabel: {
+    fontSize: 12,
+    display: 'flex',
+    top: 10,
+  },
+  circularIconContainer: {
+    top: -10,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+    borderWidth: 5,
+    borderColor: '#f9f9f9',
+  },
+  iconContainer: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  activeTab: {
+    borderWidth: 3,
+    borderColor: '#f9f9f9',
+    borderTopColor: Colors.primary,
+  },
+});
