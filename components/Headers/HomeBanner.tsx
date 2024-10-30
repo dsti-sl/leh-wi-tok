b860cd255488e456eb712a168bee9bd2c3df63fd
ShekuMunu <munusheku@gmail.com>
2024-10-30 18:06:11 +0000

    implement home screen header

--- components/Headers/HomeBanner.tsx
+++ components/Headers/HomeBanner.tsx
@@ -1,30 +1,100 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ImageSourcePropType,
+  TouchableOpacity,
+} from 'react-native';
+
+import Badge from '../../assets/images/badge.png';
+import Bell from '../../assets/images/bell.png';
+import HandShake from '../../assets/images/Handshake.png';
+
+import { Colors } from '@/constants/Colors';
+import { Record } from '@/lib/types';
 
 interface BannerProps {
   title: string;
+  user: Record;
 }
-export const HomeBanner = ({ title }: BannerProps) => {
-  /* TODO:
-    - Implement Banner component
-    - Three level depth component
-    - one for home, one for lessions, one for messages and one for others
-  */
+export const HomeBanner = ({ user }: BannerProps) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.titleTxt}>{title}</Text>
+      <View style={styles.contentContainer}>
+        <View style={styles.userWelcomeContainer}>
+          <Text style={styles.userTxt}>Hello, {user.firstName as string}</Text>
+          <Image
+            source={HandShake as ImageSourcePropType}
+            style={styles.imageStyles}
+          />
+        </View>
+        <View style={styles.iconButtons}>
+          <TouchableOpacity
+            onPress={() => {
+              // TODO: Go to notification screen
+              console.log('Notification clicked');
+            }}
+          >
+            <Image
+              style={styles.imageStyles}
+              source={Bell as ImageSourcePropType}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              // TODO: Badge implementation
+              console.log('Badge clicked');
+            }}
+          >
+            <Image
+              style={styles.imageStyles}
+              source={Badge as ImageSourcePropType}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Text style={styles.footerTxt}>Let us learn with leh we talk</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    left: -16,
+    paddingHorizontal: 20,
+    paddingTop: 5,
+    paddingBottom: 60,
+    backgroundColor: Colors.primary,
   },
-  titleTxt: {
+  contentContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  userWelcomeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 3,
+  },
+  userTxt: {
+    fontSize: 30,
+    fontFamily: 'poppins',
+    fontWeight: '700',
     color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 20,
+  },
+  iconButtons: {
+    flexDirection: 'row',
+    gap: 5,
+  },
+  imageStyles: {
+    width: 40,
+    height: 40,
+  },
+  footerTxt: {
+    fontFamily: 'poppins',
+    color: '#fff',
+    fontWeight: '400',
   },
 });
