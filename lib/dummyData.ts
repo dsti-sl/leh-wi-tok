e53059b2c9804aaed738510e274d407944ed8319
ShekuMunu <munusheku@gmail.com>
2024-10-31 10:40:55 +0000

    add dummy data

---
+++ lib/dummyData.ts
@@ -0,0 +1,16 @@
+export const tutorialsData = [
+  {
+    id: 1,
+    title: 'Introduction',
+    description: 'Learn your way around the basics of sign language',
+    video: 'https://www.youtube.com/watch?v=0FcwzMq4iWg',
+    thumbnail:
+      'https://fastly.picsum.photos/id/314/200/300.jpg?hmac=JrR8RW6cKgMfQOxlavDFHrFShwcnB_nuYpi1FWAzsgU',
+    category: 'Alphabet',
+    level: 'Beginner',
+    duration: '1:00:00',
+    rating: 4.5,
+    totalRatings: 100,
+    totalStudents: 1000,
+  },
+];
