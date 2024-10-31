458f7e442cdbfcf38150103353566e5834474413
ShekuMunu <munusheku@gmail.com>
2024-10-31 11:06:21 +0000

    update home screen

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, Image, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { HomeBanner } from '@/components/Headers/HomeBanner';
+import { HomeBanner } from '@/components/home/HomeBanner';
+import InitialVideoCard from '@/components/home/InitialVideoCard';
+import useTutorials from '@/hooks/useTutorials';
+import { Record } from '@/lib/types';
 
 const index = () => {
+  const { defaultTutorial, user } = useTutorials();
+
+  if (!user) return;
   return (
     <View style={styles.container}>
+      {/* Banner */}
       <HomeBanner user={user as Record} />
+
+      {/* Video container */}
+      {defaultTutorial && (
+        <InitialVideoCard defaultTutorial={defaultTutorial as Record} />
+      )}
+
       <Text>Home</Text>
     </View>
   );
@@ -19,6 +32,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
