7ee14b60fc2d79de43418df6fb3d76bdfd915816
ShekuMunu <munusheku@gmail.com>
2024-10-31 10:39:07 +0000

    add home components

---
+++ components/home/InitialVideoCard.tsx
@@ -0,0 +1,76 @@
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+
+import { Colors } from '@/constants/Colors';
+import { Record } from '@/lib/types';
+
+interface InitialVideoCardProps {
+  defaultTutorial: Record;
+}
+const InitialVideoCard: React.FC<InitialVideoCardProps> = ({
+  defaultTutorial,
+}) => {
+  return (
+    <View style={styles.videoContainer}>
+      <Image
+        source={{ uri: defaultTutorial.thumbnail as string }}
+        style={{ width: 60, height: 60, borderRadius: 15 }}
+      />
+      <View style={styles.videoDetails}>
+        <Text style={[styles.txtBold]}>{defaultTutorial.title as string}</Text>
+        <Text style={[styles.txtDescription]}>
+          {defaultTutorial.description as string}
+        </Text>
+        <TouchableOpacity
+          style={styles.playBtn}
+          onPress={() => {
+            // TODO: Play in modal or a dedicated screen
+            console.log('Playing preview video');
+          }}
+        >
+          <Ionicons name="play-outline" size={30} color={Colors.primary} />
+          <Text style={styles.playTxt}>Play Video</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default InitialVideoCard;
+
+const styles = StyleSheet.create({
+  videoContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    top: -35,
+    width: '90%',
+    borderColor: Colors.secondary,
+    borderWidth: 1,
+    borderRadius: 10,
+
+    flexDirection: 'row',
+    gap: 10,
+  },
+  videoDetails: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  txtDescription: {
+    opacity: 0.5,
+  },
+  playBtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+    color: Colors.primary,
+  },
+  txtBold: {
+    fontWeight: '400',
+    fontSize: 20,
+  },
+  playTxt: {
+    fontSize: 20,
+    color: Colors.primary,
+  },
+});
