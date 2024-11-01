6e234fab94f639fa7e7299ad7ec48286654ebc87
ShekuMunu <munusheku@gmail.com>
2024-11-01 19:37:01 +0000

    add const data

---
+++ constants/Data.ts
@@ -0,0 +1,14 @@
+// Fixed App Data Object Variables
+
+export const HELP_SEEKING_REASONS = [
+  'Learning basic sign language',
+  'Improving sign language fluency',
+  'Communicating with deaf family members',
+  'Interpreting for deaf friends',
+  'Understanding sign language grammar',
+  'Preparing for a sign language certification',
+  'Learning sign language for professional use',
+  'Accessing sign language resources',
+  'Participating in deaf community events',
+  'Supporting deaf and hard-of-hearing individuals',
+];
