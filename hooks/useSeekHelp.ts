e4be5ab30033306d24435d6def4ab9485a014efb
ShekuMunu <munusheku@gmail.com>
2024-11-02 11:55:21 +0000

    updae use help hook

--- hooks/useSeekHelp.ts
+++ hooks/useSeekHelp.ts
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 
-import { HELP_SEEKING_REASONS } from '@/constants/Data';
+import { HELP_SEEKING_REASONS, HELP_SEEKING_TYPES } from '@/constants/Data';
 import { Record } from '@/lib/types';
 import { parseArrayStringsToSelectableObjects } from '@/utils';
 
 const useSeekHelp = () => {
   const [helpReasons, setHelpReasons] = useState<Record[] | null>(null);
   const [selectedReason, setSelectedReason] = useState<Record>({});
+  const [helpTypes, setHelpTypes] = useState<Record[] | null>(null);
+  const [selectedHelpType, setSelectedHelpType] = useState<Record>({});
+  const [helpDescription, setHelpDescription] = useState<string>('');
 
   useEffect(() => {
     if (helpReasons) return;
@@ -14,10 +17,21 @@ const useSeekHelp = () => {
     setHelpReasons(reasons);
   }, []);
 
+  useEffect(() => {
+    if (helpTypes) return;
+    const types = parseArrayStringsToSelectableObjects(HELP_SEEKING_TYPES);
+    setHelpTypes(types);
+  }, []);
+
   return {
     helpReasons,
     selectedReason,
     setSelectedReason,
+    helpTypes,
+    selectedHelpType,
+    setSelectedHelpType,
+    helpDescription,
+    setHelpDescription,
   };
 };
 
