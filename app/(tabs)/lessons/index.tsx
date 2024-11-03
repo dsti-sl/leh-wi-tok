6f38b89d2c703dafb204d5a9f451ec23115a26ff
ShekuMunu <munusheku@gmail.com>
2024-11-03 16:49:05 +0000

    update lesson screen

--- app/(tabs)/lessons/index.tsx
+++ app/(tabs)/lessons/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import CurrentLevelProgressCard from '@/components/lessons/CurrentLevelProgressCard';
+import { LessonsBanner } from '@/components/lessons/LessonsBanner';
+import LessonsCategory from '@/components/lessons/LessonsCategory';
+import useLessons from '@/hooks/useLessons';
 
 const index = () => {
+  const { progressSummary } = useLessons();
+
   return (
     <View style={styles.container}>
-      <Text>Lessons</Text>
+      {/* Banner */}
+      <LessonsBanner />
+
+      {/* Current Level Progress */}
+      <CurrentLevelProgressCard />
+
+      {/* Lesssons Categories cards listing */}
+      {progressSummary && <LessonsCategory progressSummary={progressSummary} />}
     </View>
   );
 };
@@ -16,6 +30,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    gap: 20,
   },
 });
